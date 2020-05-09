b910e4b81bbcb5589a628d82cca2e75f86981abc
lily39 <61435797+lily39@users.noreply.github.com>
2020-05-09 09:49:38 -0500

    Add files via upload

---
+++ miscript.js
@@ -0,0 +1,7 @@
+var inputNum =Number(prompt('ingresa un numero entero mayor de 10 y menor de 20'));
+if((inputNum <10) ||  (inputNum > 20)){
+    alert('ese numero no es mayor de 10 ni menor de 20');
+
+}else{
+    alert('correcto, el numero es valido');    }
+document.write('<h1>hola mundo</h1>');
